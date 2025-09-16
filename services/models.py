2ddd45a9083063c70e6e6a33020d3b611479c653
imExtazy <nikolaykopylov2005@gmail.com>
2025-09-17 01:10:22 +0300

    added first model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+    class Service(models.Model):
+        name = models.CharField(max_length=255)
+        description = models.TextField()
+        image = models.ImageField(upload_to='services/')
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
 
+        def __str__(self):
+            return self.name
 # Create your models here.
